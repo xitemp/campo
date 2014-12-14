19dae919f33c7c9e6f1aacdd5f40e32e169de4bd
xitemp <discourselove@gmail.com>
2014-12-14 17:31:29 +0800

    update calc hot

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -15,8 +15,9 @@ class Topic < ActiveRecord::Base
   after_touch :update_hot
 
   def calculate_hot
-    order = Math.log10([likes_count, 1].max)
-    order + created_at.to_f / 45000
+    #order = Math.log10([likes_count, 1].max)
+    #order + created_at.to_f / 45000
+    1 + likes_count
   end
 
   def update_hot
